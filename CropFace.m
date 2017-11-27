c69da8ae7239e270b423e9b9cf15a1564b6e448b
Jacob Andersson <jacob.m.andersson@gmail.com>
2017-11-27 14:16:34 +0100

    Bug fix in CropFace

--- CropFace.m
+++ CropFace.m
@@ -2,21 +2,12 @@ function croppedImage = CropFace(im, rotationPoint)
     diff = [230, 160];
     rp = [rotationPoint.y, rotationPoint.x];
     
-    width = 2 * diff(2);
     height = 2 * diff(1);
+    width = 2 * diff(2);
     
     cropMinBoundary = rp - diff;
     cropMinBoundary(cropMinBoundary < 0) = 0;
     
-    croppedImage = imcrop(im, [cropMinBoundary(1), cropMinBoundary(2), width, height]);
-    
-%     temp = im;
-%     mask = zeros(size(temp));
-%     mask(cropMinBoundary(1):(cropMinBoundary(1) + height), cropMinBoundary(2):(cropMinBoundary(2) + width), :) = 1;
-%     
-%     temp = im2double(temp) .* mask;
-%     
-%     figure
-%     imshow(temp);
+    croppedImage = imcrop(im, [cropMinBoundary(2), cropMinBoundary(1), width, height]);
 end
 
